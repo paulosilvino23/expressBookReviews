a3ab70aba01d4f5140a9bd74bc007de170a179d9
Paulo S <38899836+paulosilvino23@users.noreply.github.com>
2024-02-20 22:08:35 -0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,38 +34,74 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
+// ---------------------------------------
 // Get the book list available in the shop
+// ---------------------------------------
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+
+  const newPromise = new Promise((resolve, reject) => {
+
+    try {
+      resolve(res.send(JSON.stringify(books,null,4)));
+    }
+    catch (err) {
+      reject(err);
+    }
+  }).then();
+
 });
 
+// ---------------------------------------
 // Get book details based on ISBN
+// ---------------------------------------
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn])
+
+  const newPromise = new Promise((resolve, reject) => {
+    try {
+      const isbn = req.params.isbn;
+      resolve(res.send(books[isbn]));
+    }
+    catch (err) {
+      reject(err);
+    }
+  }).then();
  });
   
+// ---------------------------------------
 // Get book details based on author
+// ---------------------------------------
 public_users.get('/author/:author',function (req, res) {
 
-  const author = req.params.author;
-
-  var filteredBooks = Object.entries(books).filter(([k,v]) => v.author === author);
-
-  res.send(filteredBooks);
+  const newPromise = new Promise((resolve, reject) => {
+    try {
+      const author = req.params.author;
+      resolve(res.send(Object.entries(books).filter(([k,v]) => v.author === author)));
+    }
+    catch (err) {
+      reject(err);
+    }
+  }).then();
 });
 
+// ---------------------------------------
 // Get all books based on title
+// ---------------------------------------
 public_users.get('/title/:title',function (req, res) {
 
-  const title = req.params.title;
-
-  var filteredBooks = Object.entries(books).filter(([k,v]) => v.title === title);
-
-  res.send(filteredBooks);
+  const newPromise = new Promise((resolve, reject) => {
+    try {
+      const title = req.params.title;
+      resolve(res.send(Object.entries(books).filter(([k,v]) => v.title === title)));
+    }
+    catch (err) {
+      reject(err);
+    }
+  }).then();
 });
 
+// ---------------------------------------
 //  Get book review
+// ---------------------------------------
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   const review = books[isbn].reviews;
